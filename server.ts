822de42d91b8e288853d714bbc319f503f837945
Yuma Soerianto <hi@madebyyuma.com>
2025-07-10 23:11:00 +1000

    Added rate limiting

--- server.ts
+++ server.ts
@@ -2,10 +2,20 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import paymentRouter from "./store.js";
+import RateLimit from "express-rate-limit";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Rate limit 3000 requests per minute
+// Landing page makes ~40 requests per load,
+// so this is equivalent to 75 page loads/min
+app.use(RateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 3000,
+    message: "You've been rate limited! Please try again later."
+}));
 
 // Absolute path to the website build output
 const websiteDir = path.resolve(__dirname, '../website');
