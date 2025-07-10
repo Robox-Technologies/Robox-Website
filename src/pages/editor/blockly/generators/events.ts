ae20dc7aa7648857c399b9e0f0217de78c982731
Cowbilet <sebgtaylor@gmail.com>
2025-07-10 17:39:13 +1000

    fixed the code uploading

--- src/pages/editor/blockly/generators/events.ts
+++ src/pages/editor/blockly/generators/events.ts
@@ -1,9 +1,8 @@
 
 import { pythonGenerator } from 'blockly/python'
-
+pythonGenerator.INDENT = '    ';
 pythonGenerator.forBlock['event_begin'] = function (block, generator) {
     const event_code = generator.statementToCode(block, 'event_code');
-    const code = `def event_begin():\n
-    global left_motor_polarity, right_motor_polarity\n${event_code}`;
+    const code = `def event_begin():\n    global left_motor_polarity, right_motor_polarity\n${event_code}`;
     return code;
 };
