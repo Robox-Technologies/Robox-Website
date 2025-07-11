73b271840e40268bee89a9eba55b93372d9a4fa0
Yuma Soerianto <hi@madebyyuma.com>
2025-07-12 00:17:07 +1000

    moved git pull outside of bash script

--- deploy.bash
+++ deploy.bash
@@ -7,5 +7,3 @@ npx tsc -p tsconfig.server.json
 webpack --config build/server/webpack.config.js
 pm2 delete server 2> /dev/null || true
 pm2 start ./build/server/server.js --name "server"
-
-git pull
