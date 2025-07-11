3d7159079078dfed06226a860cef1b24df0acc9e
Yuma Soerianto <hi@madebyyuma.com>
2025-07-11 14:01:44 +1000

    Refactor saveCart functionality

--- src/root/cart.ts
+++ src/root/cart.ts
@@ -13,13 +13,27 @@ export async function getProducts(): Promise<Record<string, Product>> {
     return await (await fetch("/api/store/products")).json()
 }
 export function getCart(): Cart {
-    let cart = localStorage.getItem("cart")
-    if (!cart) {
-        cart = JSON.stringify({quantity: 0, products: {}})
-        localStorage.setItem("cart", cart)
+    let cart: Cart;
+
+    try {
+        const rawCart = localStorage.getItem("cart");
+        cart = JSON.parse(rawCart);
+    } catch (error) {
+        console.warn("Couldn't fetch cart from localStorage: ", error);
     }
-    return JSON.parse(cart)
+
+    if (!cart) {
+        cart = { quantity: 0, products: {} };
+        saveCart(cart);
+    }
+
+    return cart;
 }
+
+function saveCart(cart: Cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
 export function getItem(product: string): { quantity: number; data: Product } | undefined {
     const cart = getCart()
     return cart.products[product]
@@ -33,7 +47,7 @@ export function refreshCart() {
         else quantity += products[product].quantity
     }
     cart.quantity = quantity
-    localStorage.setItem("cart", JSON.stringify(cart))
+    saveCart(cart);
     
     const cartElement = document.getElementById("cart")
     const cartElementText = cartElement?.querySelector("p");
@@ -56,24 +70,27 @@ export function removeCartItem(product: string) {
     const cart = getCart()
     cart.quantity -= cart.products[product].quantity
     delete cart.products[product]
-    localStorage.setItem("cart", JSON.stringify(cart))
+    saveCart(cart);
     refreshCart()
 }
 export function wipeCart() {
-    localStorage.setItem("cart", JSON.stringify({quantity: 0, products: {}}))
-    refreshCart()
+    saveCart({ quantity: 0, products: {} });
+    refreshCart();
 }
 export function addCartItem(product: string, quantity: number, cache: Product) {
     const cart = getCart();
     const item = cart.products[product]
-    if (item) item.quantity += quantity
-    else {
-        cart.products[product] = {"quantity": quantity, "data": cache}
+
+    if (item) {
+        item.quantity += quantity
+    } else {
+        cart.products[product] = { "quantity": quantity, "data": cache }
     }
-    cart.products[product].data = cache
-    cart.quantity += quantity
-    localStorage.setItem("cart", JSON.stringify(cart))
-    refreshCart()
+    
+    cart.products[product].data = cache;
+    cart.quantity += quantity;
+    saveCart(cart);
+    refreshCart();
 }
 export function setCartItem(product: string, quantity: number, cache: Product) {
     const cart = getCart()
@@ -88,6 +105,6 @@ export function setCartItem(product: string, quantity: number, cache: Product) {
     
     cart.products[product] = item;
     cart.quantity += quantity;
-    localStorage.setItem("cart", JSON.stringify(cart));
+    saveCart(cart);
     refreshCart();
 }
