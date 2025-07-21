39186490a8863dadbf23957582d83ac6495f39e2
Cowbilet <sebgtaylor@gmail.com>
2025-07-21 22:59:32 +1000

    added bundle analyzer

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -6,10 +6,49 @@ import TerserPlugin from 'terser-webpack-plugin';
 
 export default async () => {
     const { base } = await createBaseConfig();
-
+    const plugins = [];
+    if (process.env.ANALYZE_BUNDLE === 'true') {
+        const { BundleAnalyzerPlugin } = await import('webpack-bundle-analyzer');
+        plugins.push(new BundleAnalyzerPlugin({
+            analyzerMode: 'server',
+            openAnalyzer: true,
+        }));
+    }
     return merge(base, {
         mode: 'production',
         devtool: false,
+        optimization: {
+            minimize: true,
+            concatenateModules: true,
+            minimizer: [
+                new TerserPlugin({
+                    parallel: true,
+                    terserOptions: {
+                        compress: { drop_console: true },  // Remove console logs in prod
+                    },
+                }),
+                new CssMinimizerPlugin(),
+            ],
+            splitChunks: {
+                chunks: 'all',
+                cacheGroups: {
+                    vendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    },
+                },
+            },
+        },
+        plugins: [
+            ...plugins,
+        ],
+        performance: {
+            hints: 'warning',
+            maxEntrypointSize: 512000, // 500 KB limit for entry points
+            maxAssetSize: 512000,
+        },
         // Optional: performance hints, minifiers, etc.
     });
+    
 };
