221ff52a5755eb329a7ccb7376a508aa291601c2
Yuma Soerianto <hi@madebyyuma.com>
2025-07-24 09:32:01 +0700

    Update webpack path

--- deploy.bash
+++ deploy.bash
@@ -10,6 +10,6 @@ npm install
 # Compile and deploy
 npx tsc -p tsconfig.webpack.json
 npx tsc -p tsconfig.server.json
-npx webpack --config build/server/webpack.prod.config.js
+npx webpack --config ./build/webpack.prod.config.js
 pm2 delete server 2> /dev/null || true
 pm2 start ./build/server/server.js --name "server"
