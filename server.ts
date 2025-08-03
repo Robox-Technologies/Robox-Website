704a539b5cc48728124b5b4eddeae8629eb01f8e
Cowbilet <sebgtaylor@gmail.com>
2025-08-03 14:25:02 +1000

    fixed the build process

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import rateLimit from "express-rate-limit";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const isDev = process.env.NODE_ENV !== 'production';
+
 
 // Rate limit 3000 requests per minute
 app.use(rateLimit({
@@ -34,42 +34,14 @@ const apiRateLimit = rateLimit({
 app.use("/api/store", apiRateLimit, paymentRouter);
 app.use(express.json());
 
-if (isDev) {
-    const webpack = (await import('webpack')).default;
-    const webpackDevMiddleware = (await import('webpack-dev-middleware')).default;
+const websiteDir = path.resolve(__dirname, '../website');
+const path404 = path.join(websiteDir, '404.html');
 
-    const imported = await import('./webpack.dev.config.js');
-    const config = await imported.default();  // <-- call the async function to get config object
-    const compiler = webpack(config);
-    const devMiddleware = webpackDevMiddleware(compiler, {
-            publicPath: config.output.publicPath || '/',
-            stats: 'minimal'
-        })
-    app.use(
-        devMiddleware
-    );
-    devMiddleware.waitUntilValid(() => {
-        const fs = devMiddleware.context.outputFileSystem
-        const notFoundFile = fs.readFileSync(path.join(compiler.outputPath, '404.html')).toString("utf-8");
+app.use("/", express.static(websiteDir));
 
-        app.get('*', (_, res) => {
-            res.status(404).send(notFoundFile);
-        });
-    });
-} else {
-    const websiteDir = path.resolve(__dirname, '../website');
-    const path404 = path.join(websiteDir, '404.html');
-
-    app.use("/", express.static(websiteDir));
-
-    app.get('*', (_, res) => {
-        res.sendFile(path404);
-    });
-
-    app.use((_, res) => {
-        res.status(404).sendFile(path404);
-    });
-}
+app.get('*', (_, res) => {
+    res.sendFile(path404);
+});
 
 app.listen(3000, function () {
     console.log('Ro/Box website listening on port 3000!\n');
--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,8 +1,6 @@
 // webpack.dev.ts
 import { merge } from 'webpack-merge';
-import LiveReloadPlugin from 'webpack-livereload-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import WebpackBar from 'webpackbar';
 import { createBaseConfig } from './webpack.base.config.js';
 
 export default async () => {
@@ -18,8 +16,6 @@ export default async () => {
         },
         plugins: [
             new ForkTsCheckerWebpackPlugin(),
-            new WebpackBar({ name: '🚀 Building Ro/Box' }),
-            new LiveReloadPlugin({ appendScriptTag: true, delay: 300 }),
         ],
     });
 };
