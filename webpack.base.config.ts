1d22380fcd093778bd3a3432ff0b113ec42f406b
Cowbilet <sebgtaylor@gmail.com>
2025-08-10 11:20:27 +1000

    fixed the bug with wrong blog path

--- webpack.base.config.ts
+++ webpack.base.config.ts
@@ -145,7 +145,7 @@ export const createBaseConfig = async (): Promise<{ base: Configuration, product
     }
     createPages(caseStudiesPages, 'src/templates/views/articles/case-study/case-study.html', caseStudiesData);
     const blogPages = fs.readdirSync(path.resolve(__dirname, 'src/pages/blog/blogs'))
-    const blogPagesOutput = blogPages.map(file => `./src/pages/blog/${file}`);
+    const blogPagesOutput = blogPages.map(file => `./src/pages/articles/${file}`);
     const blogContent: Record<string, TemplateData> = {};
     for (const page of blogPages) {
         blogContent[path.parse(page).name] = { article: `src/pages/blog/blogs/${page}` };
