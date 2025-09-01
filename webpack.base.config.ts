fe9e78f6c7bf63c00b8ce0745738fdd315ecaac9
Cowbilet <sebgtaylor@gmail.com>
2025-09-01 15:54:14 +1000

    Merge branch 'master' of https://github.com/Robox-Technologies/Robox-Website-2 into develop

--- webpack.base.config.ts
+++ webpack.base.config.ts
@@ -9,7 +9,7 @@ import { RoboxProcessor } from './roboxProcessor.js';
 import { getProductList } from './stripe-server-helper.js';
 import { Product } from '~types/api.js';
 import { TemplateData, TemplatePage } from './types/webpack.js';
-import { ArticleLocation, convertSlateToHtml, getCMSResources } from './cms.js';
+import { ArticleLocation, convertSlateToHtml, getCMSResources } from './CMS.js';
 
 
 
