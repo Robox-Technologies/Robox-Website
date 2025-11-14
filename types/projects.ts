3c7b56991dc66cf1aa7ac332bde0229c8ee589f9
Cowbilet <sebgtaylor@gmail.com>
2025-11-14 15:27:29 +1100

    added (placeholder) extension names

--- types/projects.ts
+++ types/projects.ts
@@ -1,8 +1,17 @@
 import dayjs from "dayjs"
+const enum ExtensionType {
+    SERVO,
+    ADVANCED,
+    DISPLAY,
+}
+type Extension = {
+    [key in ExtensionType]: boolean
+}
 export type Project = {
     name: string
     time: dayjs.Dayjs
     workspace: {[key: string]: unknown} | false
     thumbnail: string
+    extensions: Extension
 }
 export type Projects = {[uuid: string]: Project} 
