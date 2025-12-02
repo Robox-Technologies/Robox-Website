bb0413ca75b7d1a357aa8372efc19bd4d1eb477d
Cowbilet <sebgtaylor@gmail.com>
2025-12-02 18:16:30 +0800

    fixed headerlink styling

--- src/components/global/header/HeaderLink.tsx
+++ src/components/global/header/HeaderLink.tsx
@@ -1,6 +1,6 @@
-export default function HeaderLink({ href, children }: { href: string; children: React.ReactNode }) {
+export default function HeaderLink({ icon, href, className, children }: { icon?: React.ReactNode; href: string; className?: string; children?: React.ReactNode }) {
     return (
-        <a href={href} className="button-standard">
+        <a href={href} className={`button-standard hover:cursor-pointer text-black bg-inherit ${className}`}>
             {children}
         </a>
     );
