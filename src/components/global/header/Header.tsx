092d088f467a6382cf075d79cabf3b0c12f6df3a
Cowbilet <sebgtaylor@gmail.com>
2025-12-02 17:33:50 +0800

    fixed the header

--- src/components/global/header/Header.tsx
+++ src/components/global/header/Header.tsx
@@ -4,12 +4,22 @@ const HeaderHeight = 64;
 export default function Header() {
     return (
         <header className="sticky top-0 z-50 flex items-center justify-between bg-primary w-full" style={{ height: HeaderHeight }}>
-            <nav className="container mx-auto flex items-center flex-row">
-                <a href="/" className="flex items-center mr-4 h-24">
-                    <Logo className='h-16'/>
-                </a>
-                <div>
+            <nav className="mx-16 flex items-center flex-row w-full font-medium text-lg" >
+                <div className='flex flex-row items-center space-x-6'> 
+                    <a href="/" className="flex items-center p-4">
+                        <Logo className="h-8 w-auto" />
+                    </a>
                     <HeaderLink href="/">Ro/Box</HeaderLink>
+                    <HeaderLink href="/">Ro/Box</HeaderLink>
+
+                </div>
+                <div className="links ml-auto flex items-center space-x-6 text-white">
+                    <HeaderLink href="/">Ro/Box</HeaderLink>
+                    <HeaderLink href="/">Ro/Box</HeaderLink>
+                    <HeaderLink href="/">Ro/Box</HeaderLink>
+                    <HeaderLink href="/">Ro/Box</HeaderLink>
+                    <HeaderLink href="/">Ro/Box</HeaderLink>
+
                 </div>
             </nav>
         </header>
