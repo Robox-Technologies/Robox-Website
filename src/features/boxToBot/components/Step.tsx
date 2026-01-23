e425defbc0eb118148ed738a7178244bf8503841
Cowbilet <sebgtaylor@gmail.com>
2026-01-23 17:44:22 +1100

    mostly fixed boxtobot

--- src/features/boxToBot/components/Step.tsx
+++ src/features/boxToBot/components/Step.tsx
@@ -1,17 +1,27 @@
+import step1Image from '@images/boxToBot/step1.svg';
+import step2Image from '@images/boxToBot/step2.svg';
+import step3Image from '@images/boxToBot/step3.svg';
+import step4Image from '@images/boxToBot/step4.svg';
+const stepImages = [step1Image, step2Image, step3Image, step4Image];
 interface StepProps {
     stepNumber: number;
     title: string;
     description: string;
+    image: ImageMetadata;
 }
 const stepColours = ['bg-blue', 'bg-red', 'bg-green', 'bg-yellow']
-export default function Step({ stepNumber, title, description }: StepProps) {
+export default function Step({ stepNumber, title, description, image }: StepProps) {
     return (
-        <div className=" flex items-center gap-8 mt-[max(35vh,150px)]">
-            <StepNumber stepNumber={stepNumber} />
-            <div className=" flex flex-col gap-4">
-                <h3 className=" text-3xl font-semibold text-white">{title}</h3>
-                <p className=" text-white text-lg">{description}</p>
+        <div className="step flex flex-col items-center gap-6 mt-[calc(max(45vh,250px))]" data-image={image.src}>
+            <div className="flex flex-row gap-6 items-start">
+                <StepNumber stepNumber={stepNumber} />
+                <div className=" flex flex-col gap-4">
+                    <h3 className=" text-3xl font-semibold text-white">{title}</h3>
+                    <p className=" text-white text-lg">{description}</p>
+                </div>
+
             </div>
+            <img src={image.src} alt={`Step ${stepNumber} Illustration`} className="lg:hidden block w-full object-contain"/>
         </div>
     )
 }
