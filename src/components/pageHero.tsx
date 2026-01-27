bf631b8bb70ab442abfad125005fc439ce2fdc45
Cowbilet <sebgtaylor@gmail.com>
2026-01-27 13:15:42 +1100

    added Ro/Box Hub resources

--- src/components/pageHero.tsx
+++ src/components/pageHero.tsx
@@ -1,29 +1,27 @@
 import Clipper from "./Clipper";
 interface PageHeroProps {
-    left: React.ReactNode;
-    right: React.ReactNode;
-    title: string;
+    left?: React.ReactNode;
+    right?: React.ReactNode;
     className?: string;
     gradient?: 'positive' | 'negative';
     overlap?: string;
     lineWidth?: string;
 }
-const leftWidth = "50%";
-const rightWidth = "50%";
-export default function PageHero({ left, right, title, className, gradient = "positive", overlap="10vw", lineWidth = "10px" }: PageHeroProps) {
+const leftWidth = "33%";
+const rightWidth = "67%";
+export default function PageHero(props: PageHeroProps) {
+    const { left, right, className, gradient = "positive", overlap="10vw", lineWidth = "10px" } = props;
     const isGradientNegative = gradient === 'negative';
     const sign = isGradientNegative ? 1 : -1;
 
     const finalLeftWidth  = `calc(${leftWidth} - ${lineWidth} / 2 + ${overlap} / 2)`;
     const finalRightWidth = `calc(${rightWidth} - ${lineWidth} / 2 + ${overlap} / 2)`;
     const finalRightOffset = `calc(${leftWidth} + ${lineWidth} / 2 - ${overlap} / 2)`;
-
+    // TODO: Make this responsive
     return (
-        <section className={`page-hero relative w-full h-[60vh] min-h-[300px] overflow-hidden ${className} `}>
+        <section className={`page-hero relative w-full h-80 overflow-hidden ${className} `}>
             <Clipper className="h-full absolute" side={"right"} gradient={isGradientNegative ? 'negative' : 'positive'} style={{ width: finalLeftWidth}} overhang={overlap}>
-                <div className="bg-blue h-full w-full" >
-                    {left}
-                </div>
+                {left}
             </Clipper>
             <Clipper className="h-full absolute" side={'left'} gradient={isGradientNegative ? 'negative' : 'positive'} style={{ width: finalRightWidth, left: finalRightOffset }} overhang={overlap}>
                 {right}
