dfbf40cea6e2c94251f7b08a8a9186e6c764223c
Cowbilet <sebgtaylor@gmail.com>
2026-01-28 13:51:31 +1100

    made the footer slant responsive

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,8 +8,8 @@ import FooterLine from "./footerLine";
 export default function Footer() {
     return (
         <footer className="w-full border-t-2 border-gray-300 h-85 flex flex-col items-center gap-4 ">
-            <div className="absolute w-125 h-85 right-0">
-                <Clipper side="left" gradient="positive" className="w-125 bg-red h-full" overhang="80px" />
+            <div className="absolute w-1/4 h-85 right-0">
+                <Clipper side="left" gradient="positive" className="w-full bg-red h-full" overhang="3vw" />
             </div>
             <div className="w-full px-12 py-10 flex flex-row justify-start gap-40">
                 <div className="flex flex-col gap-4">
@@ -38,8 +38,16 @@ export default function Footer() {
                 </div>
             </div>
             <FooterLine />
-            <div>
-
+            <div className="w-full px-12 flex flex-row justify-between z-10">
+                <p className="text-nowrap">
+                    © {new Date().getFullYear()} Ro/Box. All rights reserved.
+                </p>
+                <p className="text-nowrap">
+                    Contact us at <a href="mailto:hello@robox.com.au" className="underline">hello@robox.com.au</a>
+                </p>
+                <a href="/privacy-policy" className="underline text-white text-nowrap">
+                    Privacy Policy
+                </a>
             </div>
         </footer>
     );
--- src/components/footer/footerLine.tsx
+++ src/components/footer/footerLine.tsx
@@ -1,15 +1,15 @@
-const lineOffset = "13px";
+const lineOffset = "0.5vw";
 
 export default function FooterLine() {
     const style: React.CSSProperties = {"--line-offset": lineOffset} as React.CSSProperties;
     return (
         <hr
         style={style}
-        className="relative w-11/12 mx-auto h-0.5 bg-black border-0
+        className="relative w-11/12 mx-auto p-px bg-black border-0
                     before:content-['']
                     before:absolute before:top-0 before:right-0
-                    before:h-0.5
-                    before:w-[calc(500px-var(--line-offset)-4.166vw)]
+                    before:p-px before:h-full
+                    before:w-[calc(25vw-var(--line-offset)-4.166vw)]
                     before:bg-white"
         />
     );
