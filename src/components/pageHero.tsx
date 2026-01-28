3538ebf080ec419309cdebff59eaa0acb5deaaba
Cowbilet <sebgtaylor@gmail.com>
2026-01-28 14:50:52 +1100

    created teacher paeg

--- src/components/pageHero.tsx
+++ src/components/pageHero.tsx
@@ -2,20 +2,20 @@ import Clipper from "./Clipper";
 interface PageHeroProps {
     left?: React.ReactNode;
     right?: React.ReactNode;
+    leftWidth?: string;
     className?: string;
     gradient?: 'positive' | 'negative';
     overlap?: string;
     lineWidth?: string;
 }
-const leftWidth = "33%";
-const rightWidth = "67%";
+
 export default function PageHero(props: PageHeroProps) {
-    const { left, right, className, gradient = "positive", overlap="10vw", lineWidth = "10px" } = props;
+    const { left, right, className, gradient = "positive", leftWidth = "33%", overlap="10vw", lineWidth = "10px" } = props;
     const isGradientNegative = gradient === 'negative';
     const sign = isGradientNegative ? 1 : -1;
 
     const finalLeftWidth  = `calc(${leftWidth} - ${lineWidth} / 2 + ${overlap} / 2)`;
-    const finalRightWidth = `calc(${rightWidth} - ${lineWidth} / 2 + ${overlap} / 2)`;
+    const finalRightWidth = `calc(100% - ${leftWidth} - ${lineWidth} / 2 + ${overlap} / 2)`;
     const finalRightOffset = `calc(${leftWidth} + ${lineWidth} / 2 - ${overlap} / 2)`;
     // TODO: Make this responsive
     return (
