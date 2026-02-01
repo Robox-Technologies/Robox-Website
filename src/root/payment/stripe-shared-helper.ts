15091c1a984140516d7bf8622eb53b9b4448dbfc
Yuma Soerianto <hi@madebyyuma.com>
2026-02-01 10:54:40 -0800

    Added coupon functionality, ui, and email modification

--- src/root/payment/stripe-shared-helper.ts
+++ src/root/payment/stripe-shared-helper.ts
@@ -1,28 +1,41 @@
+import { truncate } from "fs/promises";
 import { Product } from "../../../types/api.js";
 
 const backendExecution = typeof window === 'undefined';
+const domesticCountryCode = "AU";
 
 let calculatePostage;
 if (backendExecution) {
     calculatePostage = (await import( /* webpackIgnore: true */'../../../auspost-server-helper.js')).calculatePostage;
 }
 
-export async function calculateTotalCost(cart: Record<string, number>, products: Record<string, Product>, shippingInfo: { country: string; postcode: string } | null = null): Promise<{ displayProducts: string; displayShipping: string; displayTotal: string; shipping: number; total: number; shippingSucceeded: boolean }> {
+export async function calculateTotalCost(
+    cart: Record<string, number>,
+    products: Record<string, Product>, 
+    shippingInfo: { country: string; postcode: string } | null = null,
+    discountInfo: { amountOff: number, percentOff: number, whitelistProducts: string[] | null } | null = null): 
+    Promise<{ displayProducts: string; displayShipping: string; displayDiscount: string; displayTotal: string; shipping: number; discount: number; total: number; shippingSucceeded: boolean; discountStatus: number; }> {
     let totalCost = 0;
     let totalWeight = 0;
     let totalUnitVolume = 0;
+    let discountCost = 0;
     for (const [productId, quantity] of Object.entries(cart)) {
         const product = products[productId];
         if (!product) continue;
 
-        totalCost += product.price * quantity;
+        let productCostSubtotal = product.price * quantity;
+        if (discountInfo && (!discountInfo.whitelistProducts || discountInfo.whitelistProducts.includes(productId))) {
+            discountCost += productCostSubtotal * (discountInfo.percentOff / 100);
+        }
+
+        totalCost += productCostSubtotal;
         totalWeight += product.weight * quantity;
         totalUnitVolume += product.unitVolume * quantity;
     }
     
     let shippingCost = 0;
     let shippingSucceeded = false;
-    if (shippingInfo) {
+    if (shippingInfo && (domesticCountryCode != shippingInfo.country || shippingInfo.postcode)) {
         try {
             if (backendExecution && calculatePostage) {
                 // Running on server
@@ -38,14 +51,32 @@ export async function calculateTotalCost(cart: Record<string, number>, products:
         }
     }
 
-    const finalCost = totalCost + shippingCost; 
+    let finalCost = totalCost + shippingCost;
+
+    let discountStatus = -1;
+    if (discountInfo) {
+        // Note: we can not have a final cost less than 50c else it causes an error with Stripe payments
+        discountCost += discountInfo.amountOff;
+        discountCost = Math.floor(Math.min(discountCost, finalCost - 50));
+        finalCost -= discountCost;
+        discountStatus = 1;
+
+        // If discount is 0, coupon was valid but no discount was applied.
+        if (discountCost == 0) {
+            discountStatus = -2;
+        }
+    }
+
     return {
         displayProducts: formatPrice(totalCost, true),
         displayShipping: formatPrice(shippingCost, true),
-        displayTotal: formatPrice(totalCost + shippingCost, true),
+        displayDiscount: formatPrice(discountCost, true),
+        displayTotal: formatPrice(finalCost, true),
         shipping: shippingCost,
+        discount: discountCost,
         total: finalCost,
-        shippingSucceeded: shippingSucceeded
+        shippingSucceeded: shippingSucceeded,
+        discountStatus: discountStatus
     };
 }
 
