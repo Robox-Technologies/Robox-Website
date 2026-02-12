ef05e9ee5924d41541f6f070fe04459bfef9706a
Cowbilet <sebgtaylor@gmail.com>
2026-02-12 15:19:31 +1100

    removed pointer events from the button holder

--- src/features/blockEditor/components/editorButtons/mainButton.tsx
+++ src/features/blockEditor/components/editorButtons/mainButton.tsx
@@ -35,7 +35,7 @@ export default function MainButton() {
 
     const handleClick = stateClickHandlers[connectionStatus]
     return (
-        <Button className={`rounded-3xl box-shadow w-65 text-xl font-bold py-3 px-2 ${className}`} onClick={handleClick}  disabled={!handleClick}>
+        <Button className={`rounded-3xl box-shadow w-65 text-xl font-bold py-3 px-2 pointer-events-auto ${className}`} onClick={handleClick}  disabled={!handleClick}>
             {children}
         </Button>
     )
--- src/features/blockEditor/components/editorButtons/terminalButton.tsx
+++ src/features/blockEditor/components/editorButtons/terminalButton.tsx
@@ -9,7 +9,7 @@ export default function TerminalButton() {
 
     return (
         <Button icon={faTerminal} id="terminalButton" 
-            className={`flex items-center w-10 h-10 justify-center border-2 rounded-full bg-white box-shadow 
+            className={`flex items-center w-10 h-10 justify-center border-2 rounded-full bg-white box-shadow pointer-events-auto 
                 ${isConnected ? "border-green" : "border-gray-500"}`
             } 
             iconStyle={`text-xl
