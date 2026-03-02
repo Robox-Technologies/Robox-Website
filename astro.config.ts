04b2362f7c7ed8a087bb12e2897365b924038a63
Cowbilet <sebgtaylor@gmail.com>
2026-03-02 17:26:07 +1100

    got the cart buttons working

--- astro.config.ts
+++ astro.config.ts
@@ -2,14 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 export default defineConfig({
     srcDir: "src",
     integrations: [react()],
 
     vite: {
+        // @ts-expect-error - This is going to be an issue until Astro V6 with Vite 7
         plugins: [tailwindcss(), svgr()]
     }
 });
--- src/features/cart/components/addQuantity.tsx
+++ src/features/cart/components/addQuantity.tsx
@@ -1,10 +1,12 @@
 import Button from "@components/button";
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { addToCart } from "@utils/client/cart.client";
 import clsx from "clsx";
 import { useState } from "react";
 import type { FormHTMLAttributes } from "react";
 export default function AddQuantity({className, ...props}: FormHTMLAttributes<HTMLFormElement>) {
     const [quantity, setQuantity] = useState(0);
+    
     const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setQuantity(Math.abs(parseInt(e.target.value) || 0));
     }
@@ -14,10 +16,15 @@ export default function AddQuantity({className, ...props}: FormHTMLAttributes<HT
     const decrement = () => {
         setQuantity(prev => Math.max(0, prev - 1));
     }
+    const onSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const productId = (e.currentTarget as HTMLFormElement).getAttribute("data-product-id") || "";
+        addToCart(productId, quantity);
+    }
     return (
-        <form {...props} className={clsx(className, `flex flex-row items-center gap-0 w-64`)}>
+        <form {...props} onSubmit={onSubmit} className={clsx(className, `flex flex-row items-center gap-0 w-64`)}>
             <Button type="button" icon={faMinus} iconStyle="text-black" className="rounded-l-full! border-2 p-0! w-10 border-black flex items-center justify-center h-10" onClick={decrement} />
-            <input type="number" className="w-26 text-center border-y-2 border-black h-10 no-spinner" value={quantity.toString()} min={0} onChange={onChangeHandler} />
+            <input type="number" className="w-30 text-xl text-center border-y-2 border-black h-10 no-spinner" value={quantity.toString()} min={0} onChange={onChangeHandler} />
             <Button type="button" icon={faPlus} iconStyle="text-black" className="rounded-r-full! border-2 border-black p-0! w-10 flex text-center items-center justify-center h-10" onClick={increment} />
         </form>
     )
