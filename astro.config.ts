02de0533cc26b5cbc4d43826ed7d3ac136466e4c
Cowbilet <sebgtaylor@gmail.com>
2026-03-04 11:56:34 +1100

    added proper styling for the markdown

--- astro.config.ts
+++ astro.config.ts
@@ -4,12 +4,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import RoboxSectionize from './astro/integrations/markdown/roboxSectionize';
+import mdx from '@astrojs/mdx';
 export default defineConfig({
     srcDir: "src",
-    integrations: [react()],
+    integrations: [react(), mdx()],
 
     vite: {
         // @ts-expect-error - This is going to be an issue until Astro V6 with Vite 7
         plugins: [tailwindcss(), svgr()]
+    },
+    markdown: {
+        remarkPlugins: [
+            RoboxSectionize
+        ]
     }
 });
