4e6796057d919fdd0d6c66282d659945410abf01
Cowbilet <sebgtaylor@gmail.com>
2026-03-30 18:40:19 +1100

    fixed the button issue hopefully

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     integrations: [react(), mdx()],
 
     vite: {
-        plugins: [tailwindcss(), svgr()]
+        plugins: [tailwindcss(), svgr()],
     },
     markdown: {
         remarkPlugins: [
