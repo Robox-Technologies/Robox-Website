7e50cfb92802fbed39721e1e4b31cc650f6ea955
Cowbilet <sebgtaylor@gmail.com>
2026-04-17 15:44:35 +1000

    added back the rounded a links

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -25,7 +25,7 @@ export default function Button({
             <a
                 href={href}
                 className={twMerge(
-                    `text-white px-4 py-2 hover:bg-blue-dark transition`,
+                    `text-white px-4 py-2 rounded-lg hover:bg-blue-dark transition`,
                     className,
                 )}
                 {...anchorProps}
