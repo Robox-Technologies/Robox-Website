7c39bb3c63c84bd1c3e30f7badde9b5e97c9c7b0
Cowbilet <sebgtaylor@gmail.com>
2026-05-11 09:27:02 +1000

    set everything up

--- src/features/blockEditor/components/editorButtons/mainButton.tsx
+++ src/features/blockEditor/components/editorButtons/mainButton.tsx
@@ -50,7 +50,7 @@ export default function MainButton() {
     } = usePico()
     const { className, children } = statusStyling[connectionStatus]
     useEffect(() => {
-        setCommunicationMethod('WebBluetooth')
+        setCommunicationMethod('iOSBluetooth')
     }, [])
     const stateClickHandlers: Partial<Record<ConnectionStatus, () => void>> = {
         [ConnectionStatus.DISCONNECTED]: () => {
