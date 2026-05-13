0f7f63ccc596996a317f89da0d92a8541f779ee3
Yuma Soerianto <hi@madebyyuma.com>
2026-05-13 00:08:33 -0700

    Additional changes from previous commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
     appId: 'com.robox.app',
-    appName: 'Robox Learning App',
+    appName: 'Ro/Box',
     webDir: 'dist',
 }
 
