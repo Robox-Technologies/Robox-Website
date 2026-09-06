879836ae2620ea1d7c48ceb69bd1281854e95857
Cowbilet <sebgtaylor@gmail.com>
2026-09-06 19:32:17 +1000

    added warning to flashDevice

--- src/features/studentHub/studentProjects/components/importButton.tsx
+++ src/features/studentHub/studentProjects/components/importButton.tsx
@@ -39,7 +39,7 @@ export default function ImportButton() {
         const onDrop = (event: DragEvent) => {
             event.preventDefault()
             setDragging(false)
-            void impor6tFile(event.dataTransfer?.files?.[0])
+            void importFile(event.dataTransfer?.files?.[0])
         }
 
         document.addEventListener('dragenter', onDragEnter)
